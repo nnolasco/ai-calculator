4e2e65b0bd5b2ef78b74344864dce81fd595464e
nnolasco <nnolasco@advarion.com>
2023-04-15 00:54:12 -0500

    better calculator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
-import React from 'react';
-import Calculator from './Calculator';
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+const [input, setInput] = useState('');
+const [result, setResult] = useState('');
+
+const handleClick = (value) => {
+setInput((prevInput) => prevInput + value);
+};
+
+const handleClear = () => {
+setInput('');
+setResult('');
+};
+
+const handleCalculate = () => {
+try {
+setResult(eval(input));
+} catch (error) {
+setResult('Error');
+}
+};
+
+return (
+<div className="App">
+<h1>Online Calculator</h1>
+<div className="calculator">
+<input type="text" value={input} readOnly />
+<div className="result">{result}</div>
+<div className="buttons">
+<button onClick={() => handleClick('1')}>1</button>
+<button onClick={() => handleClick('2')}>2</button>
+<button onClick={() => handleClick('3')}>3</button>
+<button onClick={() => handleClick('+')}>+</button>
+<button onClick={() => handleClick('4')}>4</button>
+<button onClick={() => handleClick('5')}>5</button>
+<button onClick={() => handleClick('6')}>6</button>
+<button onClick={() => handleClick('-')}>-</button>
+<button onClick={() => handleClick('7')}>7</button>
+<button onClick={() => handleClick('8')}>8</button>
+<button onClick={() => handleClick('9')}>9</button>
+<button onClick={() => handleClick('')}></button>
+<button onClick={() => handleClick('0')}>0</button>
+<button onClick={() => handleClick('.')}>.</button>
+<button onClick={handleClear}>C</button>
+<button onClick={() => handleClick('/')}>/</button>
+<button onClick={handleCalculate}>=</button>
+</div>
+</div>
+</div>
+);
 }
 
 export default App;
